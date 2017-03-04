537e8bc0121262a32c8b59389859682f640354b4
Nat York <natyork@gmail.com>
2017-03-04 10:24:57 -0800

    Add our coord to dangerzone

--- move.js
+++ move.js
@@ -7,6 +7,87 @@ var startData = {
   game_id: "abc123"
 }
 
+var moveData = {
+  "you": "25229082-f0d7-4315-8c52-6b0ff23fb1fb",
+  "width": 2,
+  "turn": 0,
+  "snakes": [
+    {
+      "taunt": "git gud",
+      "name": "my-snake",
+      "id": "25229082-f0d7-4315-8c52-6b0ff23fb1fb",
+      "health_points": 93,
+      "coords": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ]
+    },
+    {
+      "taunt": "gotta go fast",
+      "name": "other-snake",
+      "id": "0fd33b05-37dd-419e-b44f-af9936a0a00c",
+      "health_points": 50,
+      "coords": [
+        [
+          1,
+          0
+        ],
+        [
+          1,
+          0
+        ],
+        [
+          1,
+          0
+        ]
+      ]
+    }
+  ],
+  "height": 2,
+  "game_id": "870d6d79-93bf-4941-8d9e-944bee131167",
+  "food": [
+    [
+      1,
+      1
+    ]
+  ],
+  "dead_snakes": [
+    {
+      "taunt": "gotta go fast",
+      "name": "other-snake",
+      "id": "c4e48602-197e-40b2-80af-8f89ba005ee9",
+      "health_points": 50,
+      "coords": [
+        [
+          5,
+          0
+        ],
+        [
+          5,
+          0
+        ],
+        [
+          5,
+          0
+        ]
+      ]
+    }
+  ]
+}
+
+
+
+
 function walls(data) {
   var maxX = data.width;
   var maxY = data.height;
@@ -28,6 +109,11 @@ function walls(data) {
 }
 
 walls(startData)
+
+function start(data) {
+  var startData = JSON.parse(data)
+  return startData
+}
 
 function move(data) {
   var dataJSON = JSON.parse(data);
@@ -55,8 +141,8 @@ function murderSnake(dataJSON) {
 
 // return the object for our snake
 function getMySnake(dataJSON) {
-  snakes = dataJSON.snakes
-  you = dataJSON.you
+  var snakes = dataJSON.snakes
+  var you = dataJSON.you
   for (var i = 0; i < snakes.length; i++){
     if (snakes[i].id === you) {
       return snakes[i]
@@ -95,19 +181,24 @@ function addSnakesToDANGERZONE(dataJSON){
   return dangerSnakes
 }
 
-function dangerZone(data) {
+function dangerZone(start, move) {
 // danger zones
+  var danger_zones = []
+  var ourCoords = getMySnake(move)
+  var us = ourCoords.coords
+
+  console.log("our coord: ", us)
+
+
+
+
 //  1 - if space occupied by snake including your own body
 //  2 - if space surrounded on 3 sides
 //  3 - if space is a wall
+// 4 -  last coord on a
 
-
-
+return danger_zones
 }
 
-// fs.readFile(filePath, function (err, data) {
-//    if (err) {
-//       return console.error(err);
-//    }
-//    move(data.toString());
-// });
+dangerZone (startData, moveData)
+
