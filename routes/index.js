ab4ecd5bf482a6adc2b63c595cbe02094769c8c3
Nat York <natyork@gmail.com>
2017-03-04 16:58:20 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -34,12 +34,13 @@ router.post('/move', function (req, res) {
 
   // Response data
 
-  var data = {
-    move: 'right', // one of: ['up','down','left','right']
-    taunt: 'Outta my way, snake!', // optional, but encouraged!
-  }
+  // var data = {
+  //   move: 'right', // one of: ['up','down','left','right']
+  //   taunt: 'Outta my way, snake!', // optional, but encouraged!
+  // }
 
-  // var data = move.move(req.body)
+
+  var data = move.move(JSON.parse(req.body))
 
   return res.json(data);
 });
